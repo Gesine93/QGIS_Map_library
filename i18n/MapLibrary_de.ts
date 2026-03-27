4095b8bed8da69a1c9038ba31fde5f6e3e8a1265
Gesine <110592817+Gesine93@users.noreply.github.com>
2026-03-27 15:42:59 +0100

    Add files via upload

---
+++ i18n/MapLibrary_de.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="2.0">
+<context>
+    <name>MapLibrary</name>
+    <message>
+        <location filename="../map_library.py" line="664"/>
+        <source>permission denied</source>
+        <translation>keine Berechtigung</translation>
+    </message>
+    <message>
+        <location filename="../map_library.py" line="665"/>
+        <source>no SELECT privilege</source>
+        <translation>kein SELECT-Recht</translation>
+    </message>
+    <message>
+        <location filename="../map_library.py" line="687"/>
+        <source>The layer</source>
+        <translation>Der Layer</translation>
+    </message>
+    <message>
+        <location filename="../map_library.py" line="688"/>
+        <source>could not be loaded because you do not have sufficient privileges</source>
+        <translation>konnte nicht geladen werden, weil Sie die benötigten Rechte dafür nicht besitzen</translation>
+    </message>
+</context>
+</TS>
